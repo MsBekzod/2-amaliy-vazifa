6df3c9c5c830855ca71644d8b62caef1fe27a776
MsBekzod <bekzodmuzaffarovich@example.com>
2024-04-06 15:14:42 +0500

    UYGA VAZIFA: 2-amaliy-mazifa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Kalkulyator.
+System.Console.WriteLine("Salom, men barcha arifmetik amallarni hisoblab bera olaman! Xohlasangiz sinab ko'ramiz....");
+System.Console.WriteLine("Misol:");
+string misol1 = Console.ReadLine();
+string misol2 = Console.ReadLine();
+string amal = Console.ReadLine();
+decimal misoll1 = Convert.ToInt32(misol1);
+decimal misoll2 = Convert.ToInt32(misol2);
+decimal amall = Convert.ToInt32(amal);
+decimal yechim = misoll1 amall misoll2; 
+
+System.Console.WriteLine($"Javob: {yechim}");
